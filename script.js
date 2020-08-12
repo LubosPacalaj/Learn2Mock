04045daa4d0174778af6bee7d478666b54135a96
Lubos Pacalaj <pacalaj@ambitas.sk>
2020-08-12 11:45:52 +0200

    update project

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ const learn2mock = new Learn2Mock();
 todoButton.addEventListener('click', () => {
    
      if(todosInput.value !== "") { 
-    const todos = learn2mock.createTodos(Number(todosInput.value));
-      // console.log('Todos',todos)
-    const serializingTodo = learn2mock.serialize(todos)
-    const exportTodo = learn2mock.exportTodos(serializingTodo);
+        const todos = learn2mock.createTodos(Number(todosInput.value));
+          // console.log('Todos',todos)
+        const serializingTodo = learn2mock.serialize(todos)
+        const exportTodo = learn2mock.exportTodos(serializingTodo);
 
-    todosInput.value = "";
-    todoList.innerText = `${serializingTodo.length} Todos ${exportTodo} `
-    console.log(`Todos ${serializingTodo.length}`,exportTodo) 
+        todosInput.value = "";
+        console.log(`Todos ${serializingTodo.length}`,exportTodo) 
+        todoList.innerText = `${serializingTodo.length} Todos ${exportTodo} `
+
     }
 })
